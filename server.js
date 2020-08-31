edc2da30973f4769fd92c50fb14b1aa6beda93b4
Sumudu Siriwardana <Sumudu@ppmp.com.au>
2020-08-31 14:03:46 +1000

    re comment the server code

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require('path');
-const http = require("http");
+//const http = require("http");
 const app = express();
-const server = http.createServer(app);
-const socket = require("socket.io");
-const io = socket(server);
+//const server = http.createServer(app);
+//const socket = require("socket.io");
+//const io = socket(server);
 
 app.use(express.static(__dirname + '/dist/angular-test'));
 app.get('/*', function(req,res){
@@ -12,59 +12,51 @@ app.get('/*', function(req,res){
     '/dist/angular-test/index.html'));
 });
 
-const users = {};
+// const users = {};
 
-const socketToRoom = {};
+// const socketToRoom = {};
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-});
+// io.on('connection', socket => {
 
-io.on('connection', socket => {
+//     socket.on("join room", roomID => {
+//         //console.log("user joioned for room id: " + roomID);
+//         if (users[roomID]) {
+//             const length = users[roomID].length;
+//             if (length === 4) {
+//                 socket.emit("room full");
+//                 return;
+//             }
+//             users[roomID].push(socket.id);
+//         } else {
+//             users[roomID] = [socket.id];
+//         }
+//         socketToRoom[socket.id] = roomID;
+//         const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+//         socket.emit("all users", usersInThisRoom);
+//     });
 
-    socket.on("join room", roomID => {
-        //console.log("user joioned for room id: " + roomID);
-        if (users[roomID]) {
-            const length = users[roomID].length;
-            if (length === 4) {
-                socket.emit("room full");
-                return;
-            }
-            users[roomID].push(socket.id);
-        } else {
-            users[roomID] = [socket.id];
-        }
-        socketToRoom[socket.id] = roomID;
-        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-        socket.emit("all users", usersInThisRoom);
-    });
+//     socket.on("sending signal", payload => {
+//         io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+//     });
 
-    socket.on("sending signal", payload => {
-        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-    });
+//     socket.on("returning signal", payload => {
+//         io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+//     });
 
-    socket.on("returning signal", payload => {
-        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-    });
+//     socket.on('disconnect', () => {
+//         const roomID = socketToRoom[socket.id];
+//         let room = users[roomID];
+//         if (room) {
+//             room = room.filter(id => id !== socket.id);
+//             users[roomID] = room;
+//             console.log("user disconnected")
+//             // socket.emit('user-disconnected', socket.id)
+//         }
+//     });
 
-    socket.on('disconnect', () => {
-        const roomID = socketToRoom[socket.id];
-        let room = users[roomID];
-        if (room) {
-            room = room.filter(id => id !== socket.id);
-            users[roomID] = room;
-            console.log("user disconnected")
-            // socket.emit('user-disconnected', socket.id)
-        }
-    });
+// });
 
-});
-
-server.listen(3000, () => console.log('server is running on port 3000'));
+// server.listen(3000, () => console.log('server is running on port 3000'));
 app.listen(process.env.PORT ||8080);
 
 
