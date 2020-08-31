9b22b8f6c4692567fc2e63b468ea47e2394073e6
Sumudu Siriwardana <Sumudu@ppmp.com.au>
2020-08-31 14:44:53 +1000

    server update 1

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const path = require('path');
-//const http = require("http");
+const http = require("http");
 const app = express();
-//const server = http.createServer(app);
-//const socket = require("socket.io");
-//const io = socket(server);
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
 app.use(express.static(__dirname + '/dist/angular-test'));
 app.get('/*', function(req,res){
