2cae73162728f3cc68733b5d956ec264f0a154b2
Sumudu Siriwardana <Sumudu@ppmp.com.au>
2020-08-31 14:50:35 +1000

    server update 2

--- server.js
+++ server.js
@@ -12,49 +12,49 @@ app.get('/*', function(req,res){
     '/dist/angular-test/index.html'));
 });
 
-// const users = {};
+const users = {};
 
-// const socketToRoom = {};
+const socketToRoom = {};
 
-// io.on('connection', socket => {
+io.on('connection', socket => {
 
-//     socket.on("join room", roomID => {
-//         //console.log("user joioned for room id: " + roomID);
-//         if (users[roomID]) {
-//             const length = users[roomID].length;
-//             if (length === 4) {
-//                 socket.emit("room full");
-//                 return;
-//             }
-//             users[roomID].push(socket.id);
-//         } else {
-//             users[roomID] = [socket.id];
-//         }
-//         socketToRoom[socket.id] = roomID;
-//         const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-//         socket.emit("all users", usersInThisRoom);
-//     });
+    socket.on("join room", roomID => {
+        //console.log("user joioned for room id: " + roomID);
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+        socket.emit("all users", usersInThisRoom);
+    });
 
-//     socket.on("sending signal", payload => {
-//         io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-//     });
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
 
-//     socket.on("returning signal", payload => {
-//         io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-//     });
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
 
-//     socket.on('disconnect', () => {
-//         const roomID = socketToRoom[socket.id];
-//         let room = users[roomID];
-//         if (room) {
-//             room = room.filter(id => id !== socket.id);
-//             users[roomID] = room;
-//             console.log("user disconnected")
-//             // socket.emit('user-disconnected', socket.id)
-//         }
-//     });
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+            console.log("user disconnected")
+            // socket.emit('user-disconnected', socket.id)
+        }
+    });
 
-// });
+});
 
 // server.listen(3000, () => console.log('server is running on port 3000'));
 app.listen(process.env.PORT ||8080);
