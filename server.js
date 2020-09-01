b8adef3fd2eb5c18d42a8e452703c33e23f325cf
Sumudu Siriwardana <Sumudu@ppmp.com.au>
2020-09-01 10:08:16 +1000

    cc

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ io.on('connection', socket => {
 });
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 server.listen(port, host, function() {
   console.log("Server started.......");
