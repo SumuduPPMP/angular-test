90253b5d826759b24f585f831b6feeebc9d7f20b
Sumudu Siriwardana <Sumudu@ppmp.com.au>
2020-09-02 16:50:28 +1000

    service commit

--- src/app/web-socket.service.ts
+++ src/app/web-socket.service.ts
@@ -1,31 +1,31 @@
-import { Injectable } from '@angular/core';
-import * as io from "socket.io-client";
-import { Observable, Subscriber } from 'rxjs';
+// import { Injectable } from '@angular/core';
+// import * as io from "socket.io-client";
+// import { Observable, Subscriber } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class WebSocketService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class WebSocketService {
 
-  socket:any;
-  uri:string ="ws://localhost:3000"
-  constructor() {
-    this.socket = io(this.uri);
-  }
+//   socket:any;
+//   uri:string ="ws://localhost:3000"
+//   constructor() {
+//     this.socket = io(this.uri);
+//   }
 
-  listen(eventName: String){
-    return new Observable((subscriber) =>{
+//   listen(eventName: String){
+//     return new Observable((subscriber) =>{
 
-      this.socket.on(eventName,(data) =>{
-        subscriber.next(data);
-        console.log("rtrtrttttttttttt")
-      })
-    })
-  }
-  // emit(eventName: string, data1: any, data2:any) {
-  //   this.socket.emit(eventName,data1,data2)
-  // }
-  emit(eventName: string, data1: any){
-    this.socket.emit(eventName,data1)
-  }
-}
+//       this.socket.on(eventName,(data) =>{
+//         subscriber.next(data);
+//         console.log("rtrtrttttttttttt")
+//       })
+//     })
+//   }
+//   // emit(eventName: string, data1: any, data2:any) {
+//   //   this.socket.emit(eventName,data1,data2)
+//   // }
+//   emit(eventName: string, data1: any){
+//     this.socket.emit(eventName,data1)
+//   }
+// }
