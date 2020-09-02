2133c6b919345c5652702dc648cc89a532024100
Sumudu Siriwardana <Sumudu@ppmp.com.au>
2020-09-02 10:46:03 +1000

    comment the socket code

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const path = require('path');
 const http = require("http");
-const server = express();
-//const server = http.createServer(app);
+const app = express();
+const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
 
-server.use(express.static(__dirname + '/dist/angular-test'));
-server.get('/*', function(req,res){
+app.use(express.static(__dirname + '/dist/angular-test'));
+app.get('/*', function(req,res){
   res.sendFile(path.join(__dirname+
     '/dist/angular-test/index.html'));
 });
@@ -17,44 +17,47 @@ const users = {};
 const socketToRoom = {};
 io.on('connection', socket => {
 
-  socket.emit("test","testing 1 2 3");
-    socket.on("join room", roomID => {
-        console.log("user joioned for room id: " + roomID);
-        if (users[roomID]) {
-            const length = users[roomID].length;
-            if (length === 4) {
-                socket.emit("room full");
-                return;
-            }
-            users[roomID].push(socket.id);
-        } else {
-            users[roomID] = [socket.id];
-        }
-        socketToRoom[socket.id] = roomID;
-        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-        socket.emit("all users", usersInThisRoom);
-    });
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  // socket.emit("test","testing 1 2 3");
+  //   socket.on("join room", roomID => {
+  //       console.log("user joioned for room id: " + roomID);
+  //       if (users[roomID]) {
+  //           const length = users[roomID].length;
+  //           if (length === 4) {
+  //               socket.emit("room full");
+  //               return;
+  //           }
+  //           users[roomID].push(socket.id);
+  //       } else {
+  //           users[roomID] = [socket.id];
+  //       }
+  //       socketToRoom[socket.id] = roomID;
+  //       const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+  //       socket.emit("all users", usersInThisRoom);
+  //   });
 
-    socket.on("sending signal", payload => {
-        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-    });
+  //   socket.on("sending signal", payload => {
+  //       io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+  //   });
 
-    socket.on("returning signal", payload => {
-        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-    });
+  //   socket.on("returning signal", payload => {
+  //       io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+  //   });
 
-    socket.on('disconnect', () => {
-        const roomID = socketToRoom[socket.id];
-        let room = users[roomID];
-        if (room) {
-            room = room.filter(id => id !== socket.id);
-            users[roomID] = room;
-            console.log("user disconnected")
-            // socket.emit('user-disconnected', socket.id)
-        }
-    });
+  //   socket.on('disconnect', () => {
+  //       const roomID = socketToRoom[socket.id];
+  //       let room = users[roomID];
+  //       if (room) {
+  //           room = room.filter(id => id !== socket.id);
+  //           users[roomID] = room;
+  //           console.log("user disconnected")
+  //           // socket.emit('user-disconnected', socket.id)
+  //       }
+  //   });
 
 });
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
 // var port =  3001; // Use the port that Heroku provides or default to 3000
 // server.listen(port, function() {
