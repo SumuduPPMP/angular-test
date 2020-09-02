16d7b7660a9d0b8837c5b329a23f90ec8e008f98
Sumudu Siriwardana <Sumudu@ppmp.com.au>
2020-09-02 11:27:36 +1000

    server update

--- server.js
+++ server.js
@@ -15,6 +15,16 @@ app.get('/*', function(req,res){
 const users = {};
 
 const socketToRoom = {};
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
+
 io.on('connection', socket => {
 
   console.log('Client connected');
