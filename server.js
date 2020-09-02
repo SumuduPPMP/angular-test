e8d0ab46923038237a27f702d11c6dda0e0f1ea0
Sumudu Siriwardana <Sumudu@ppmp.com.au>
2020-09-02 15:37:38 +1000

    dd

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ io.on('connection', socket => {
 });
 // setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
-const port = process.env.PORT ||8080;
+const port = process.env.PORT ||3000;
  server.listen(port, () => console.log('server is running on port 8080'));
  //app.listen(process.env.PORT ||8080);
 
