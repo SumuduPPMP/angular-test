1b0d7e625f7ba0fe6ceb6b99fa44573a9da9f84b
Sumudu Siriwardana <Sumudu@ppmp.com.au>
2020-09-03 12:20:06 +1000

    difine count for user videos

--- src/app/room/room.component.ts
+++ src/app/room/room.component.ts
@@ -124,13 +124,13 @@ console.log("room is fulll")
     });
     console.log('created a peer');
     peer.on('signal', (signal) => {
-      //if (count == 0) {
+      if (count == 0) {
         this.socketRef.emit('sending signal', {
           userToSignal,
           callerID,
           signal,
         });
-      //}
+      }
       count++;
     });
 
@@ -146,9 +146,9 @@ console.log("room is fulll")
     });
 
     peer.on('signal', (signal) => {
-      //if (count == 0) {
+      if (count == 0) {
         this.socketRef.emit('returning signal', { signal, callerID });
-      //}
+      }
       count++;
     });
 
