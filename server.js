f7ff482e5c8abd4e778a64bf7cff5e6f05826a7a
Sumudu Siriwardana <Sumudu@ppmp.com.au>
2020-09-08 16:29:10 +1000

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ io.on('connection', socket => {
       let room = users[roomID];
       if (room) {
         console.log("leave call: " + user_id);
-        socket.broadcast.emit("user disconnect",user_id);
+        //socket.broadcast.emit("user disconnect",user_id);
           room = room.filter(id => id !== socket.id);
           users[roomID] = room;
       }
