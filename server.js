aa5c6bff502d1eef3c0dca7177207fdbc038968e
SumuduPPMP <67084177+SumuduPPMP@users.noreply.github.com>
2021-03-23 10:16:42 +1100

    change the maximum number of  room members

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ io.on('connection', socket => {
       console.log("roomid is: " + roomID + "  aaa: " + users[roomID]);
       if (users[roomID]) {
           const length = users[roomID].length;
-          if (length === 6) {
+          if (length === 4) {
               socket.emit("room full");
               return;
           }
